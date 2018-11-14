3fe3cc9ddfd83cce71b35bb5c71f82b437683a7e
manoguidjulius <45031167+manoguidjulius@users.noreply.github.com>
2018-11-14 01:15:05 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # data_structure_algorithm
+ Activity in Data Structures and Algorithm submitted by Julius Manoguid
---
+++ boxer[1].java
@@ -0,0 +1,25 @@
+package exercise1;
+
+import exercise1.SingularlyLinkedList;
+
+
+public class boxer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingularlyLinkedList<String> boxers= new SingularlyLinkedList <String>();
+		
+		boxers.addFirst("manoguid");
+		boxers.addFirst("Hulyo");
+		boxers.addFirst("Julius");
+		boxers.addFirst("calixtro");
+		boxers.addFirst("pagba");
+		
+		
+		
+		System.out.println("size : " + boxers.size());
+		
+	}
+
+}
+ 
